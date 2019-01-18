d157de4f236fe235e6013a7bea09ed9f696bb090
Youngju KIM <fjvbn2003@gmail.com>
2019-01-19 07:03:24 +0900

    #25 Tracking Auth State

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,23 +4,23 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import {connect} from 'react-redux'
 
-const Navbar = () =>{
+const Navbar = (props) =>{
+    const {auth} = props;
+    //console.log(auth);
+    const links = auth.uid ? <SignedInLinks/> :<SignedOutLinks/>;
     return(
         <nav className="nav wrapper blue darken-4">
             <div className="container">
-                <Link to='/' className="brand-logo left">랭킹핑퐁</Link>
-                <SignedInLinks/>
-                {/* <SignedOutLinks/> */}
-
-                
+                <Link to='/' className="brand-logo left">랭퐁</Link>
+                {links}
             </div>
         </nav>
     )
 }
 const mapStateToProps = (state) =>{
-    console.log(state);
+    // console.log(state);
     return{
-
+        auth: state.firebase.auth
     }
 }
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps)(Navbar); 
