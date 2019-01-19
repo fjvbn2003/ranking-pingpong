7cee949e4598bcdec15dc74b8d575ba588d713c8
Youngju KIM <fjvbn2003@gmail.com>
2019-01-19 13:12:17 +0900

    #37 Cloud Function Auth Trigger

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ const createNotification = (notification) => {
     return admin.firestore().collection('notifications')
         .add(notification)
         .then(doc => console.log('notification added', doc));
-}
+};
 
 // gameCreate 클라우드 함수 생성. (firestore games 문서가 추가될 때 발동)
 exports.gameCreated = functions.firestore
@@ -21,9 +21,22 @@ exports.gameCreated = functions.firestore
         const game = doc.data();
         const notification={
             content: '새로운 경기가 등록되었습니다',
-            referee_name: game.referee_name,
             result: `(${game.p1_name}) ${game.p1_score}:${game.p2_score} (${game.p2_name})`,
             time: admin.firestore.FieldValue.serverTimestamp(),
         }
         return createNotification(notification);
+    });
+
+exports.userJoined = functions.auth.user()
+    .onCreate(user =>{
+        return admin.firestore().collection('users')
+            .doc(user.uid).get().then(doc=>{
+                const newUser = doc.data();
+                const notification={
+                    content: '새로운 선수가 랭퐁에 참가하였습니다',
+                    result: `이름: ${newUser.name} , 부수: ${newUser.level} , 구장:${newUser.location}`,
+                    time: admin.firestore.FieldValue.serverTimestamp(),
+                }
+                return createNotification(notification);
+            })
     })
