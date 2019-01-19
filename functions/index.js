19ed885c2ddbbe5f0b7e0bc6ffef32706fbb0441
Youngju KIM <fjvbn2003@gmail.com>
2019-01-19 12:43:46 +0900

    #36 Cloud Function Firestore Triggers

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,29 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello 랭킹핑퐁!");
 });
+
+const createNotification = (notification) => {
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('notification added', doc));
+}
+
+// gameCreate 클라우드 함수 생성. (firestore games 문서가 추가될 때 발동)
+exports.gameCreated = functions.firestore
+    .document('games/{gameId}')
+    .onCreate(doc =>{
+        
+        const game = doc.data();
+        const notification={
+            content: '새로운 경기가 등록되었습니다',
+            referee_name: game.referee_name,
+            result: `(${game.p1_name}) ${game.p1_score}:${game.p2_score} (${game.p2_name})`,
+            time: admin.firestore.FieldValue.serverTimestamp(),
+        }
+        return createNotification(notification);
+    })
