782c138eb7418acf933347f8d93998ea5ab29ef0
Youngju KIM <fjvbn2003@gmail.com>
2019-01-21 09:28:10 +0900

    20190121 deploy

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0805491a2dee6536f4c5d75ebfe81a3a.js"
+  "/precache-manifest.50155eeeae420abcfe4e41fadc07eb55.js"
 );
 
 workbox.clientsClaim();
